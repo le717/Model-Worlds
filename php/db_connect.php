fb2902f9aa0994f25b03d7ded151d8fd9d274fdd
Triangle717 <le717.code@yahoo.com>
2015-07-23 22:26:32 -0400

    Database connection

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,15 +1,8 @@
 <?php
-  if ($_SERVER['HTTP_HOST'] === '') {
-  DEFINE('DB_USER', '');
+  DEFINE('DB_USER', 'root');
   DEFINE('DB_PASSWORD', '');
-  DEFINE('DB_HOST', 'localhost');
-  DEFINE('DB_NAME', '');
-  } else {
-    DEFINE('DB_USER', 'root');
-    DEFINE('DB_PASSWORD', '');
-    DEFINE('DB_HOST', '127.0.0.1');
-    DEFINE('DB_NAME', 'model_worlds');
-  }
+  DEFINE('DB_HOST', '127.0.0.1');
+  DEFINE('DB_NAME', 'model_worlds');
 
   $mysqli = new MySQLi(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
   if ($mysqli->connect_error) {
