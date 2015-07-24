4a837993c32a9cd1efb67acd71d114417ea5a558
Triangle717 <le717.code@yahoo.com>
2015-07-23 23:10:09 -0400

    * Rework database connection

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,13 +1,16 @@
 <?php
-  DEFINE('DB_USER', 'root');
-  DEFINE('DB_PASSWORD', '');
-  DEFINE('DB_HOST', '127.0.0.1');
-  DEFINE('DB_NAME', 'model_worlds');
+  function MW_dbConnect() {
+    $DB_USER     = 'root';
+    $DB_PASSWORD = '';
+    $DB_HOST     = '127.0.0.1';
+    $DB_NAME     = 'model_worlds';
 
-  $mysqli = new MySQLi(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-  if ($mysqli->connect_error) {
-    echo $mysqli->connect_error;
-    unset($mysqli);
-  } else {
-    $mysqli->set_charset('utf8');
+    $mysqli = new MySQLi($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+    if ($mysqli->connect_error) {
+      echo $mysqli->connect_error;
+      unset($mysqli);
+    } else {
+      $mysqli->set_charset('utf8');
+    }
+    return $mysqli;
   }
